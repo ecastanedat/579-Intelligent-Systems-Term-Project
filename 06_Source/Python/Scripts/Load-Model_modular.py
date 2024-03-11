ca057b8358ae5f0b4399760cef066b45d622d510
Luis_CT <esteban.castanedat@gmail.com>
2024-03-10 20:43:59 -0400

    General changes

--- 06_Source/Python/Scripts/Load-Model_modular.py
+++ 06_Source/Python/Scripts/Load-Model_modular.py
@@ -1,6 +1,35 @@
+'''
+/*============================================================================*/
+/*                        BA AN SOFTWARE GROUP                                */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*/
+/*
+ * $Source: Load-Model_modular.py $
+ * $Revision: 0.1$
+ * $Author: Castaneda, Luis $
+ * $Date: Mar 3, 2024 8:37:38 PM $
+ */
+/*============================================================================*/
+/*                                                                            */
+/* The reproduction, transmission, or use of this document or its content is  */
+/* not permitted without express written authority. Offenders will be liable  */
+/* for damages.                                                               */
+/* All rights, including rights created by patent grant or registration of a  */
+/* utility model or design, are reserved.                                     */
+/*                                                                            */
+/*============================================================================*/
+/* Reference documents:                                                       */
+/*                                                                            */
+/*============================================================================*/
+/* OBJECT HISTORY                                                             */
+/*============================================================================
+* $Log: Load-Model_modular.py $
+* Revision 0.1 2024/02/27 10:57 EST Castaneda, Luis
+* Initial revision
+'''
 
 import os
-import time
 import cv2
 import numpy as np
 from keras.models import load_model
@@ -8,77 +37,100 @@ from keras.preprocessing import image
 from keras.preprocessing.image import img_to_array
 from keras.applications.mobilenet_v2 import preprocess_input
 
-# Specify the file name and path to save the image
-image_path = "C:/Projects/579-Intelligent-Systems-Term-Project/06_Source/Python/Dependencies/captured_image.jpg"
 
-# Load pre-trained Facial Emotion Recognition model
-model = load_model('C:/Projects/579-Intelligent-Systems-Term-Project/06_Source/Python/Models/modelv9_Angry_Sad_Neutral.h5')  # Path to your model file
-
-
-# Function to save an image
+# **************************************************************
+# *  Name                 :  save_image
+# *  Description          :  Function to save an image.
+# *  Parameters           :  frame, file_path
+# *  Return               :  int
+# *  Critical/explanation :  No
+# **************************************************************
 def save_image(frame, file_path):
-    # Check if the file already exists
     if os.path.exists(file_path):
-        # If file exists, delete it
         os.remove(file_path)
     
-    # Save the image
     cv2.imwrite(file_path, frame)
     print("Image saved as:", file_path)
 
-# Define a function to detect faces in the image using Haar Cascade classifier
+
+# **************************************************************
+# *  Name                 :  save_image
+# *  Description          :  Define a function to detect faces in 
+#                            the image using Haar Cascade classifierFunction 
+#                            to save an image.
+# *  Parameters           :  image
+# *  Return               :  faces
+# *  Critical/explanation :  No
+# **************************************************************
 def detect_faces(image):
     # Load the pre-trained Haar Cascade classifier for face detection
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Detect faces in the image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     return faces
 
-# Continuously capture images from the webcam
-cap = cv2.VideoCapture(0)  # Use the default webcam
-time.sleep(1)
 
-ret, frame = cap.read()
-save_image(frame, image_path)
+# **************************************************************
+# *  Name                 :  predict
+# *  Description          :  1. Starts a model.
+#                            2. Captures an image.
+#                            3. Formats the img to fit the model.
+#                            4. Saves the output image into output_img_path.
+#                            to save an image.
+# *  Parameters           :  model_path, output_img_path
+# *  Return               :  predicted emotion
+# *  Critical/explanation :  No
+# **************************************************************
+def predict(model_path: str, output_img_path: str):
 
-# Detect faces in the captured frame
-faces = detect_faces(frame)
+    # Load pre-trained Facial Emotion Recognition model
+    model = load_model(model_path)
 
-# Process each detected face
-for (x, y, w, h) in faces:
+    # Open camera reference
+    cap = cv2.VideoCapture(0)  # Use the default webcam
+    ret, frame = cap.read()
 
-    # Crop the face region from the frame
-    face = frame[y:y+h, x:x+w]
+    # Detect faces in the captured frame
+    faces = detect_faces(frame)
 
-    # Resize the cropped face to 48x48
-    face_resized = cv2.resize(face, (48, 48))
+    # Process each detected face
+    for (x, y, w, h) in faces:
 
-    # Convert the cropped face to grayscale
-    face_gray = cv2.cvtColor(face_resized, cv2.COLOR_BGR2GRAY)
+        face = frame[y:y+h, x:x+w]
+        face_resized = cv2.resize(face, (48, 48))
+        face_gray = cv2.cvtColor(face_resized, cv2.COLOR_BGR2GRAY)
+        normalized = face_gray / 255.0
 
-    # Normalize pixel values to range between 0 and 1
-    normalized = face_gray / 255.0
+        # Prepare the image for the model
+        normalized = np.expand_dims(normalized, axis=-1)
+        normalized = np.expand_dims(normalized, axis=0)
 
-    # Prepare the image for the model
-    normalized = np.expand_dims(normalized, axis=-1)
-    normalized = np.expand_dims(normalized, axis=0)
+        # Use the model to predict the emotion
+        emotion_labels = ['Angry', 'Sad', 'Neutral']
+        prediction = model.predict(normalized, verbose=0)
+        predicted_emotion = emotion_labels[np.argmax(prediction)]
 
-    # Use the model to predict the emotion
-    # emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
-    emotion_labels = ['Angry', 'Sad', 'Neutral']
-    prediction = model.predict(normalized, verbose=0)
-    predicted_emotion = emotion_labels[np.argmax(prediction)]
+        cv2.putText(frame, predicted_emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Display the captured frame with the detected emotion
-    cv2.putText(frame, predicted_emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Show the captured frame
+    # cv2.imshow('Facial Emotion Recognition', frame)
+    save_image(frame, output_img_path)
 
-# Show the captured frame
-cv2.imshow('Facial Emotion Recognition', frame)
+    # Release the webcam and close OpenCV windows
+    cap.release()
+    cv2.destroyAllWindows()
 
+    return 'The predicted emotion is: ' + predicted_emotion
 
-# Release the webcam and close OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+def main():
+    # Globals
+    model_path = 'C:/Projects/579-Intelligent-Systems-Term-Project/06_Source/Python/Models/modelv9_Angry_Sad_Neutral.h5'
+    prediction_img_output_path = 'C:/Projects/579-Intelligent-Systems-Term-Project/06_Source/Python/Dependencies/captured_image.jpg'
+        
+    emotion = predict(model_path, prediction_img_output_path)
+    print(emotion)
+
+if __name__=="__main__":
+    main()
